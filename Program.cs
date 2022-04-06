f2ad7563df7a2d42cc27830810424a5d51a2dfd1
vvsugrevin <vvsugrevin@gmail.com>
2022-04-06 18:13:23 +0300

    Блок-схема алгоритма задачи

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] arr ={"apple","=)","412","rtfm","banana","for"};
+var realSize = 0;
+
+var result = new string[arr.Length];
+
+foreach (var count in arr)
+{
+    if (count.Length <= 3)
+    {
+        result[realSize] = count;
+        realSize++;
+    }
+}
+System.Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
